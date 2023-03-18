d256ddc4b096220e1643066877a2036487e82150
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2023-03-19 00:36:55 +0530

    add next-auth logic

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import { env } from "@/env";
+
+export default NextAuth({
+  providers: [
+    // OAuth authentication providers...
+
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,73 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+
+import { LoginIcon } from "@/components/Icons";
+
 export default function Home() {
+  // data contains all the user data like name, email, image, etc.
+  const { data: session } = useSession();
+
   return (
     <>
-      <form className="h-screen bg-[#000] flex justify-center items-center flex-col">
-        <h1 className="text-2xl font-semibold text-transparent sm:text-3xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          Enter your Gmail
-        </h1>
-
-        <div className="flex flex-col items-center justify-center w-full gap-3 px-10 mt-7 text-white/80">
-          <input
-            type="mail"
-            required
-            className="w-full sm:max-w-md px-4 py-2 bg-[#222222] border-none rounded-md outline-none text-[#a1a1a1]"
-          />
-          <button
-            className="py-2 rounded-md outline-none sm:py-2 focus:outline-none focus:ring-1 focus:ring-blue-400/25 bg-[#222] w-full sm:max-w-md cursor-pointer text-[#a1a1a1] hover:bg-[#2e2e2e] hover:text-[#fff] transition-colors duration-200 ease-in-out"
-            type="submit"
-          >
-            Verify
-          </button>
+      {session ? (
+        <div className="flex flex-col items-center justify-center h-screen gap-7">
+          <h1 className="text-base text-white/80 sm:text-xl">
+            Hello <span className="font-semibold">{session.user?.name}</span>👋
+          </h1>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <h2 className="text-sm sm:text-lg text-white/80">
+              signed in as{" "}
+              <span className="text-transparent pointer-events-none bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                {session.user?.email}
+              </span>
+            </h2>
+            <button
+              className="py-2 rounded-md outline-none focus:outline-none focus:ring-1 focus:ring-blue-400/25 bg-[#222] w-2/4 sm:max-w-xs cursor-pointer text-[#a1a1a1] hover:bg-[#2e2e2e] hover:text-[#fff] transition-colors duration-200 ease-in-out text-sm sm:text-base"
+              onClick={() => signOut()}
+            >
+              Log out
+            </button>
+          </div>
         </div>
-      </form>
+      ) : (
+        <div className="h-screen bg-[#000] flex justify-center items-center flex-col">
+          <div>
+            <h1 className="text-xl font-semibold sm:text-3xl text-white/80">
+              <Link
+                href="https://next-auth.js.org/"
+                target="_blank"
+                className="border-b border-b-pink-400/80"
+              >
+                next-auth
+              </Link>{" "}
+              example for Google
+            </h1>
+
+            <div className="flex flex-col items-center justify-center w-full gap-3 px-10 mt-7 text-white/80">
+              <button
+                className="py-2 rounded-md outline-none focus:outline-none focus:ring-1 focus:ring-blue-400/25 bg-[#222] w-full sm:max-w-md cursor-pointer text-[#a1a1a1] hover:bg-[#2e2e2e] hover:text-[#fff] transition-colors duration-200 ease-in-out flex items-center justify-center gap-1 text-sm sm:text-base"
+                type="submit"
+                onClick={() => signIn("google")}
+              >
+                Login via Gmail{" "}
+                <span>
+                  <LoginIcon />
+                </span>
+              </button>
+            </div>
+          </div>
+          <div className="mt-20 text-xs text-white/50 group sm:text-sm">
+            Code:{" "}
+            <Link
+              href="https://github.com/nkilm/next-auth-verify-gmail"
+              target="_blank"
+              className="transition-colors duration-200 ease-in-out border-b border-b-pink-400/30 group-hover:text-white/70"
+            >
+              https://github.com/nkilm/next-auth-verify-gmail
+            </Link>{" "}
+          </div>
+        </div>
+      )}
     </>
   );
 }
